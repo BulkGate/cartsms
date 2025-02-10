8661bb3c1dee8d7a69b67d45d539d0e35cc40457
martin <martinkreizl@gmail.com>
2025-02-10 09:59:54 +0100

    DI...

--- src/DI/Factory.php
+++ src/DI/Factory.php
@@ -3,31 +3,27 @@
 namespace BulkGate\CartSms\DI;
 
 use BulkGate\Plugin;
-use BulkGate\Plugin\Settings;
-use BulkGate\Plugin\Debug\Logger;
-use BulkGate\Plugin\Debug\Repository\LoggerSettings;
-use BulkGate\Plugin\Debug\Requirements;
-use BulkGate\Plugin\IO;
 use BulkGate\CartSms\Eshop;
-use BulkGate\Plugin\DI\{Container, FactoryStatic, Factory as DIFactory};
-use BulkGate\CartSms\Database\Connection;
+use BulkGate\CartSms\Event;
+use BulkGate\CartSms\Database;
 use Tracy\Debugger;
 
-class Factory implements DIFactory
+class Factory implements Plugin\DI\Factory
 {
-	use FactoryStatic;
+	use Plugin\DI\FactoryStatic;
 
-	protected static function createContainer(array $parameters = []): Container
+	protected static function createContainer(array $parameters = []): Plugin\DI\Container
 	{
 		bdump($parameters);
 		['registry' => $registry] = $parameters;
+		$iso = $registry->language->get('code');
 
 		$registry->load->model('localisation/order_status');
 		$registry->load->model('localisation/return_status');
 		$registry->load->model('localisation/language');
 		$registry->load->model('setting/store');
 
-		$container = new Container($parameters['mode'] ?? 'strict');
+		$container = new Plugin\DI\Container($parameters['mode'] ?? 'strict');
 
 		if (($parameters['debug'] ?? false) && class_exists(Debugger::class))
 		{
@@ -38,17 +34,17 @@ class Factory implements DIFactory
 		}
 
 		// Database
-		$container['database.connection'] = ['factory' => Connection::class, 'parameters' => ['db' => $parameters['db']]];
+		$container['database.connection'] = ['factory' => Database\Connection::class, 'parameters' => ['db' => $parameters['db']]];
 
 		// Debug
-		$container['debug.repository.logger'] = ['factory' => LoggerSettings::class, 'factory_method' => function () use ($container, $parameters): LoggerSettings
+		$container['debug.repository.logger'] = ['factory' => Plugin\Debug\Repository\LoggerSettings::class, 'factory_method' => function () use ($container, $parameters): Plugin\Debug\Repository\LoggerSettings
 		{
-			$service = new LoggerSettings($container->getByClass(Settings\Settings::class));
+			$service = new Plugin\Debug\Repository\LoggerSettings($container->getByClass(Plugin\Settings\Settings::class));
 			$service->setup(is_int($parameters['logger_limit'] ?? null) ? $parameters['logger_limit'] : 100);
 			return $service;
 		}];
-		$container['debug.logger'] = Logger::class;
-		$container['debug.requirements'] = Requirements::class;
+		$container['debug.logger'] = Plugin\Debug\Logger::class;
+		$container['debug.requirements'] = Plugin\Debug\Requirements::class;
 
 		// Eshop
 		$container['eshop.configuration'] = ['factory' => Eshop\Configuration::class, 'factory_method' => fn () => new Eshop\Configuration($parameters['module_version'], $parameters['url'], $parameters['name'] ?? 'Store')];
@@ -58,24 +54,56 @@ class Factory implements DIFactory
 		$container['eshop.language'] = ['factory' => Eshop\Language::class, 'factory_method' => fn () => new Eshop\Language($registry->model_localisation_language)];
 		$container['eshop.multistore'] = ['factory' => Eshop\MultiStore::class, 'factory_method' => fn () => new Eshop\MultiStore($container->getService('eshop.configuration'), $registry->model_setting_store)];
 
+		// Event loaders
+		$container['event.loader.extension'] = ['factory' => Event\Loader\Extension::class, 'auto_wiring' => false];
+		$container['event.loader.shop'] = ['factory' => Event\Loader\Shop::class, 'auto_wiring' => false];
+		$container['event.loader.order'] = ['factory' => Event\Loader\Order::class, 'auto_wiring' => false];
+		$container['event.loader.order_status'] = ['factory' => Event\Loader\OrderStatus::class, 'auto_wiring' => false];
+		$container['event.loader.customer'] = ['factory' => Event\Loader\Customer::class, 'auto_wiring' => false];
+		$container['event.loader.product'] = ['factory' => Event\Loader\Product::class, 'auto_wiring' => false];
+		$container['event.loader.post'] = ['factory' => Event\Loader\Post::class, 'auto_wiring' => false];
+
+		// Event
+		$container['event.hook'] = ['factory' => Plugin\Event\Hook::class, 'parameters' => ['version' => $parameters['api_version'] ?? '1.0']];
+		$container['event.asynchronous.repository'] = Plugin\Event\Repository\AsynchronousDatabase::class;
+		$container['event.asynchronous'] = Plugin\Event\Asynchronous::class;
+		$container['event.loader'] = ['factory' => Plugin\Event\Loader::class, 'factory_method' => fn () => new Plugin\Event\Loader([
+			$container->getByClass(Event\Loader\Order::class),
+			$container->getByClass(Event\Loader\OrderStatus::class),
+			$container->getByClass(Event\Loader\Customer::class),
+			$container->getByClass(Event\Loader\Shop::class),
+			$container->getByClass(Event\Loader\Product::class),
+			$container->getByClass(Event\Loader\Post::class),
+			$container->getByClass(Event\Loader\Extension::class),
+		])];
+		$container['event.dispatcher'] = Plugin\Event\Dispatcher::class;
+
 		// IO
-		$container['io.connection.factory'] = ['factory' => IO\ConnectionFactory::class, 'factory_method' => function () use ($container): IO\ConnectionFactory
+		$container['io.connection.factory'] = ['factory' => Plugin\IO\ConnectionFactory::class, 'factory_method' => function () use ($container): Plugin\IO\ConnectionFactory
 		{
 			/**
 			 * @var Eshop\Configuration $configuration
 			 */
 			$configuration = $container->getByClass(Eshop\Configuration::class);
 
-			return new IO\ConnectionFactory($configuration->url(), $configuration->product(), $container->getByClass(Settings\Settings::class));
+			return new Plugin\IO\ConnectionFactory($configuration->url(), $configuration->product(), $container->getByClass(Plugin\Settings\Settings::class));
 		}];
-		$container['io.connection'] = ['factory' => IO\Connection::class, 'factory_method' => fn () => $container->getByClass(IO\ConnectionFactory::class)->create()];
-		$container['io.url'] = ['factory' => IO\Url::class, 'parameters' => ['url' => $parameters['gate_url'] ?? 'https://portal.bulkgate.com']];
+		$container['io.connection'] = ['factory' => Plugin\IO\Connection::class, 'factory_method' => fn () => $container->getByClass(Plugin\IO\ConnectionFactory::class)->create()];
+		$container['io.url'] = ['factory' => Plugin\IO\Url::class, 'parameters' => ['url' => $parameters['gate_url'] ?? 'https://portal.bulkgate.com']];
+
+		// Localization
+		$container['localization.language'] = ['factory' => Plugin\Localization\LanguageSettings::class, 'parameters' => ['iso' => $iso]];
+		$container['localization.translator'] = Plugin\Localization\TranslatorSettings::class;
+		$container['localization.formatter'] = extension_loaded('intl') ? ['factory' => Plugin\Localization\FormatterIntl::class, 'factory_method' => fn () => new Plugin\Localization\FormatterIntl($iso)] : Plugin\Localization\FormatterBasic::class;
 
 		// Settings
-		$container['settings.repository.database'] = Settings\Repository\SettingsDatabase::class;
-		$container['settings.settings'] = Settings\Settings::class;
-		$container['settings.repository.synchronizer'] = Settings\Repository\SynchronizationDatabase::class;
-		$container['settings.synchronizer'] = Settings\Synchronizer::class;
+		$container['settings.repository.database'] = Plugin\Settings\Repository\SettingsDatabase::class;
+		$container['settings.settings'] = Plugin\Settings\Settings::class;
+		$container['settings.repository.synchronizer'] = Plugin\Settings\Repository\SynchronizationDatabase::class;
+		$container['settings.synchronizer'] = Plugin\Settings\Synchronizer::class;
+
+		// User
+		$container['user.sign'] = Plugin\User\Sign::class;
 
 		return $container;
 	}
---
+++ src/Event/Loader/Shop.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace BulkGate\CartSms\Event\Loader;
+
+use BulkGate\Plugin\Event\DataLoader;
+use BulkGate\Plugin\Event\Variables;
+
+class Shop implements DataLoader
+{
+
+	public function load(Variables $variables, array $parameters = []): void
+	{
+
+	}
+}
