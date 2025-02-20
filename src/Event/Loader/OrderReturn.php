f2f3da9839acc65f0267fcaa037bdbdd09d0766e
martin <martinkreizl@gmail.com>
2025-02-20 09:35:25 +0100

    order_return loader

---
+++ src/Event/Loader/OrderReturn.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace BulkGate\CartSms\Event\Loader;
+
+use BulkGate\Plugin;
+
+class OrderReturn implements Plugin\Event\DataLoader
+{
+	public function __construct(private $order_return_model, private Plugin\Localization\Formatter $formatter)
+	{
+	}
+
+	public function load(Plugin\Event\Variables $variables, array $parameters = []): void
+	{
+		if (!isset($variables['return_id'])) {
+			return;
+		}
+
+		$order_return = $this->order_return_model->getReturn($variables['return_id']);
+
+		$variables['return_customer_message'] = $order_return['comment'];
+		$variables['return_date'] = $this->formatter->format('date', $order_return['date_added']);
+
+		// pro customera + administratora
+		$variables['return_action'] = $order_return['action'] ?? null;
+		$variables['return_action_id'] = $order_return['return_action_id'];
+		$variables['return_reason_id'] = $order_return['return_reason_id'];
+		$variables['return_reason'] = $order_return['reason'] ?? null;
+
+		//pouze pro administratora
+		$variables['return_status'] = $order_return['return_status'] ?? null;
+		$variables['return_status_id'] = $order_return['return_status_id'];
+
+		$variables['return_products1'] = $order_return['quantity'] . 'x ' . $order_return['product'] . ' ' . $order_return['product_id'];
+		$variables['return_products2'] = $order_return['quantity'] . 'x ' . $order_return['product'];
+		$variables['return_products3'] = $order_return['quantity'] . 'x (' . $order_return['product_id'] . ') ' . $order_return['product'];
+		$variables['return_products4'] = $order_return['quantity'] . 'x ' . $order_return['product_id'];
+	}
+}
