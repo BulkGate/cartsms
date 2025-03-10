259a8284bb43a79c8a4619fd7179bb7096f6ac72
martin <martinkreizl@gmail.com>
2025-03-10 10:30:55 +0100

    shop loader tests

--- src/Event/Loader/Shop.php
+++ src/Event/Loader/Shop.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace BulkGate\CartSms\Event\Loader;
 
@@ -6,7 +6,8 @@ use BulkGate\Plugin;
 
 class Shop implements Plugin\Event\DataLoader
 {
-
+	/** @param \Opencart\Catalog\Model\Setting\Setting | \Opencart\Admin\Model\Setting\Setting $settings_model */
+	/** @param \Opencart\Catalog\Model\Localisation\Language | \Opencart\Admin\Model\Localisation\Language $language_model */
 	public function __construct(private $settings_model, private $language_model, private $request)
 	{
 	}
@@ -17,14 +18,14 @@ class Shop implements Plugin\Event\DataLoader
 			return;
 		}
 
-		$settings = $this->settings_model->getSetting('config', $variables['shop_id']);
+		$settings = $this->settings_model->getSetting('config', (int) $variables['shop_id']);
 
 		$variables['shop_email'] = $settings['config_email'];
 		$variables['shop_name'] = $settings['config_name'];
 		$variables['shop_domain'] ??= $settings['config_url'] ?? ""; //?? HTTP_CATALOG; //todo: shop 0 tuto polozku nema ... tady musime vzit aktualni url?
 		$variables['shop_currency'] = $settings['config_currency'];
 		$variables['shop_phone'] = $settings['config_telephone'];
-		$variables['lang_id'] ??= ($this->language_model->getLanguageByCode($this->request->get['language'] ?? $this->request->cookie['language']) ?: $this->language_model->getLanguageByCode($settings['config_language_catalog']))['language_id'];
+		$variables['lang_id'] ??= ($this->language_model->getLanguageByCode($this->request->get['language'] ?? $this->request->cookie['language'] ?? $settings['config_language_catalog']))['language_id'];
 		$variables['lang_iso'] = $this->language_model->getLanguage($variables['lang_id'])['code'];
 	}
 }
