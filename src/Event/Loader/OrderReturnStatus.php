3f9555836ac0e647753a4c89e1bd10bd17ee131c
martin <martinkreizl@gmail.com>
2025-03-10 09:47:39 +0100

    return status loader tests

--- src/Event/Loader/OrderReturnStatus.php
+++ src/Event/Loader/OrderReturnStatus.php
@@ -6,6 +6,7 @@ use BulkGate\Plugin;
 
 class OrderReturnStatus implements Plugin\Event\DataLoader
 {
+	/** @param \Opencart\Admin\Model\Localisation\ReturnStatus $order_return_status_model */
 	public function __construct(private $order_return_status_model)
 	{
 	}
